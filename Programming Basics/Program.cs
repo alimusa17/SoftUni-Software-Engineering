9f880e8dee7aba7bed77bbb76ec2766544b998ba
Ali Musa <106487730+alimusa17@users.noreply.github.com>
2022-06-02 20:42:59 +0300

    Added new folder via upload

---
+++ Programming Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if(grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+           // else
+           // {
+           //   Console.WriteLine("Try again!");
+           // }
+        }
+    }
+}

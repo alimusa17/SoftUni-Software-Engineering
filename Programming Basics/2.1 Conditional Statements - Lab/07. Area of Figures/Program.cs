57fb9f13d4556747eb4168ae4709450329aa0eed
Ali Musa <alimusa_@abv.bg>
2022-06-03 22:26:48 +0300

    Added new exercises to folder 2.1 Conditional Statements - Lab

---
+++ Programming Basics/2.1 Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _07._Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = 0;
+
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                area = a * a;
+                //Console.WriteLine($"{area:f3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = a * b;
+                //Console.WriteLine($"{area:f3}");
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                area = r * r * Math.PI;
+                //Console.WriteLine($"{area:f3}");
+            }
+            else if (figure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double ha = double.Parse(Console.ReadLine());
+                area = a * ha / 2;
+                //Console.WriteLine($"{area:f3}");
+            }
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}

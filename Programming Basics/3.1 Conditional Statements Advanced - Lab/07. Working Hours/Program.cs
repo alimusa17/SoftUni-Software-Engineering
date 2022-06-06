f54939da3a2e8a8bef58adcb1df66be7661c473c
Ali Musa <alimusa_@abv.bg>
2022-06-06 21:42:26 +0300

    Added new exercises to folder 3.1 Conditional Statements Advanced - Lab via GitHub Desktop

---
+++ Programming Basics/3.1 Conditional Statements Advanced - Lab/07. Working Hours/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Hours = int.Parse(Console.ReadLine());
+
+            string DayOfWeek = Console.ReadLine();
+
+            if (Hours >= 10 && Hours <= 18 && DayOfWeek != "Sunday")
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}

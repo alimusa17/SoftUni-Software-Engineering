8a80d4f32e703a48ed2c91783670aec5040c989f
Ali Musa <alimusa_@abv.bg>
2022-06-10 22:43:32 +0300

    Added new exercises to folder 3.2 Conditional Statements Advanced - Exercise

---
+++ Programming Basics/3.2 Conditional Statements Advanced - Exercise/07. Hotel Room/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _07._Hotel_Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mounth = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            if (mounth == "May" || mounth == "October")
+            {
+                studioPrice = nights * 50;
+                apartmentPrice = nights * 65;
+                if (nights > 14)
+                {
+                    apartmentPrice -= apartmentPrice * 0.10;
+                    studioPrice -= studioPrice * 0.30;
+                }
+                else if (nights > 7)
+                {
+                    studioPrice -= studioPrice * 0.05;
+                }
+            }
+            else if (mounth == "June" || mounth == "September")
+            {
+                studioPrice = nights * 75.20;
+                apartmentPrice = nights * 68.70;
+                if (nights > 14)
+                {
+                    apartmentPrice -= apartmentPrice * 0.10;
+                    studioPrice -= studioPrice * 0.20;
+                }
+            }
+            else
+            {
+                studioPrice = nights * 76;
+                apartmentPrice = nights * 77;
+                if (nights > 14)
+                {
+                    apartmentPrice -= apartmentPrice * 0.10;
+                }
+            }
+
+            Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+
+        }
+    }
+}
---
+++ Programming Basics/3.2 Conditional Statements Advanced - Exercise/09. Ski Trip/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _09._Ski_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nights = int.Parse(Console.ReadLine());
+            nights = nights - 1;
+            string roomType = Console.ReadLine();
+            string feedback = Console.ReadLine();
+
+            double result = 0;
+
+            switch (roomType)
+            {
+                case "room for one person":
+                    result = nights * 18;
+                    break;
+                case "apartment":
+                    result = nights * 25;
+                    if (nights < 10)
+                    {
+                        result *= 0.7;
+                    }
+                    else if (nights <= 15)
+                    {
+                        result *= 0.65;
+                    }
+                    else
+                    {
+                        result *= 0.5;
+                    }
+                    break;
+                case "president apartment":
+                    result = nights * 35;
+                    if (nights < 10)
+                    {
+                        result *= 0.9;
+                    }
+                    else if (nights <= 15)
+                    {
+                        result *= 0.85;
+                    }
+                    else
+                    {
+                        result *= 0.80;
+                    }
+                    break;
+                
+            }
+
+            if (feedback == "positive")
+            {
+                result *= 1.25;                     
+            }
+            else if (feedback == "negative")
+            {
+                result *= 0.9;                    
+            }
+
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}

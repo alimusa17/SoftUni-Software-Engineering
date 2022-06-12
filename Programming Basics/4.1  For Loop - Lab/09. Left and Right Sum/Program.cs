9b45dd29c6f471d53282e37cdec8c29003f8f400
Ali Musa <alimusa_@abv.bg>
2022-06-12 22:42:47 +0300

    Added new exercises to folder 4.1 For Loop - Lab

---
+++ Programming Basics/4.1  For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum1 += number;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum2 += number;
+            }
+            if (sum1 == sum2)
+            {
+                Console.WriteLine($"Yes, sum = {sum1}");
+            }
+
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(sum1 - sum2)}");
+            }
+        }
+    }
+}
---
+++ Programming Basics/4.1  For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequenceCount = int.Parse(Console.ReadLine());
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < sequenceCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum += number;
+                }
+                else
+                {
+                    oddSum += number;
+                }
+            }
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {oddSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(oddSum - evenSum)}");
+            }
+        }
+    }
+}

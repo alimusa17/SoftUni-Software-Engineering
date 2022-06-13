ff0311bafafb10f57f951d54a45b202a857d54fa
Ali Musa <alimusa_@abv.bg>
2022-06-13 22:39:04 +0300

    Created new folder 4.2 For Loop - Exercise

---
+++ Programming Basics/4.2 For Loop - Exercise/04. Clever Lily/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04._Clever_Lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+
+            double washingMachine = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            double money = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money += i * 5 - 1;
+                }
+                else
+                {
+                    money += toyPrice;
+                }
+            }
+
+            if (money >= washingMachine)
+            {
+                Console.WriteLine($"Yes! {money-washingMachine:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washingMachine - money:f2}");
+            }
+        }
+    }
+}

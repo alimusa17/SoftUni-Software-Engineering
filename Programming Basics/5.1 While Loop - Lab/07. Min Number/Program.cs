235ce7767c316b3256491b46715e0a957e56ccd8
Ali Musa <alimusa_@abv.bg>
2022-06-17 23:25:40 +0300

    Added new exercises to folder 5.1 While Loop - Lab

---
+++ Programming Basics/5.1 While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            int minNum = int.MaxValue;
+
+            while (command != "Stop")
+            {
+                int currentNum = int.Parse(command);
+                if (currentNum < minNum)
+                {
+                    minNum = currentNum;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
---
+++ Programming Basics/5.1 While Loop - Lab/08. Graduation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int grade = 1;
+            double sum = 0;
+            int counter = 0;
+
+            while (grade <= 12)
+            {
+                double yearlyGrade = double.Parse(Console.ReadLine());
+
+                if (yearlyGrade < 4)
+                {
+                    if (counter == 1)
+                    {
+                        break;
+                    }
+                    counter++;
+                    continue;
+                }
+
+                sum += yearlyGrade;
+                grade++;
+            }
+
+            double averageGrade = sum / 12;
+
+            if (grade > 12)
+            {
+                Console.WriteLine($"{name} graduated. Average grade:{averageGrade:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grade} grade");
+            }
+           
+           
+
+        }
+    }
+}

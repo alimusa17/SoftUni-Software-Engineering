c8df07a0a3db229463dd5b66d9f233b09ed7926f
Ali Musa <alimusa_@abv.bg>
2022-06-19 23:08:49 +0300

    Added new exercises to folder 5.2 While Loop - Exercise

---
+++ Programming Basics/5.2 While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int piecesCount = width * length;
+            string piecesTaken = Console.ReadLine();
+
+            while (piecesTaken != "STOP")
+            {
+                int piecesNumber = int.Parse(piecesTaken);
+                piecesCount -= piecesNumber;
+                if (piecesCount < 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(piecesCount)} pieces more.");
+                    return;
+                }
+                piecesTaken = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{piecesCount} pieces are left.");
+        }
+    }
+}
---
+++ Programming Basics/5.2 While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int volume = width * length * height;
+
+            string count = Console.ReadLine();
+
+            while (count != "Done")
+            {
+                int countNumber = int.Parse(count);
+                volume -= countNumber;
+                if (volume < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+                    return;
+                }
+                count = Console.ReadLine();
+            }
+            Console.WriteLine($"{volume} Cubic meters left.");
+        }
+    }
+}

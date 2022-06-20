5853ceaf7a5203ba4a3cee4c259e639bd1a7d2c8
Ali Musa <alimusa_@abv.bg>
2022-06-20 23:10:38 +0300

    Created new folder 6.1 Nested Loops - Lab

---
+++ Programming Basics/6.1 Nested Loops - Lab/01. Clock/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <= 23; hour++)
+            {
+                for (int min = 0; min <= 59; min++)
+                {
+                    Console.WriteLine($"{hour}:{min}");
+                }
+
+            }
+        }
+    }
+}
---
+++ Programming Basics/6.1 Nested Loops - Lab/02. Multiplication Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num1 = 1; num1 <= 10; num1++)
+            {
+                for (int num2 = 1; num2 <= 10; num2++)
+                {
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                }
+
+            }
+        }
+    }
+}

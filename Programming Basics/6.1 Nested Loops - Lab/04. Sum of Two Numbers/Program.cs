807f3f227d66a3f001f9372bf8514749430790fd
Ali Musa <alimusa_@abv.bg>
2022-06-21 22:04:03 +0300

    Added new exercises to folder 6.1 Nested Loops - Lab

---
+++ Programming Basics/6.1 Nested Loops - Lab/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    counter++;
+
+                    if (i + j == magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magicNum})");
+                        return;
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"{counter} combinations - neither equals {magicNum}");
+        }
+    }
+}

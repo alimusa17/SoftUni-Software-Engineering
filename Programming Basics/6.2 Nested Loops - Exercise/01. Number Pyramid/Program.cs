d714dc2fda6267683469d8839a7215e85993d0d6
Ali Musa <alimusa_@abv.bg>
2022-06-22 23:23:43 +0300

    Created new folder 6.2 Nested Loops - Exercise

---
+++ Programming Basics/6.2 Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNum = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(currentNum + " ");
+                    currentNum++;
+
+                    if (currentNum > n)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+
+                if (currentNum > n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Programming Basics/6.2 Nested Loops - Exercise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02._Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            int oddSum;
+            int evenSum;
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                oddSum = 0;
+                evenSum = 0;
+
+                string currentNum = i.ToString();
+
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    if (j % 2 ==0)
+                    {
+                        oddSum += currentNum[j];
+                    }
+                    else
+                    {
+                        evenSum += currentNum[j];
+                    }
+                }
+                if (oddSum == evenSum)
+                {
+                    Console.Write(currentNum + " ");
+                }
+            }
+        }
+    }
+}

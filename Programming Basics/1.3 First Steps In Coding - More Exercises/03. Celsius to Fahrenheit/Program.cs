efbc983d6afe1a0ead67c4754e1cf1610844c683
Ali Musa <alimusa_@abv.bg>
2022-06-28 22:24:03 +0300

    Created new folder 1.3 First Steps In Coding - More Exercises

---
+++ Programming Basics/1.3 First Steps In Coding - More Exercises/03. Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Celsius_to_Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = (celsius * 1.8) + 32;
+
+            Console.WriteLine($"{fahrenheit:f2}");
+
+
+
+
+        }
+    }
+}
---
+++ Programming Basics/1.3 First Steps In Coding - More Exercises/04. Vegetable Market/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Vegetable_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceKgVeg = double.Parse(Console.ReadLine());
+            double priceKgFruit = double.Parse(Console.ReadLine());
+            int totalKgVeg = int.Parse(Console.ReadLine());
+            int totalKgFruit = int.Parse(Console.ReadLine());
+
+            double priceVeg = priceKgVeg * totalKgVeg;
+            double priceFruit = priceKgFruit * totalKgFruit;
+
+            double totalSum = (priceVeg + priceFruit)/1.94;
+
+            Console.WriteLine($"{totalSum:f2}");
+
+
+        }
+    }
+}

bdd6952de2546554d5708eafee8a83c73b7498c8
Ali Musa <alimusa_@abv.bg>
2022-06-29 22:51:06 +0300

    Created new folder 2.3 Conditional Statements - More Exercises

---
+++ Programming Basics/2.3 Conditional Statements - More Exercises/02. Skeleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02._Skeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minControl = int.Parse(Console.ReadLine());
+            int secControl = int.Parse(Console.ReadLine());
+            double distanceMeters = double.Parse(Console.ReadLine());
+            int coverTime = int.Parse(Console.ReadLine());
+
+            double controlInSec = minControl * 60 + secControl;
+
+            double delay = distanceMeters / 120;
+
+            delay = delay * 2.5;
+
+            double timeMarin = (distanceMeters / 100) * coverTime - delay;
+
+            if (controlInSec >= timeMarin)
+            {
+                Console.WriteLine("Marin Bangiev won an Olympic quota!");
+                Console.WriteLine($"His time is {timeMarin:f3}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, Marin failed! He was {timeMarin - controlInSec:f3} second slower.");
+            }
+        }
+    }
+}

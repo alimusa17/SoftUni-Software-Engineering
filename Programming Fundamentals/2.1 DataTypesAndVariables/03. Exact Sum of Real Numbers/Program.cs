b7959f0215d5daffc94d5e2d05240b0daee5ff3f
Ali Musa <alimusa_@abv.bg>
2022-07-22 21:43:42 +0300

    Created new folder 2.1 Data Types And Variables

---
+++ Programming Fundamentals/2.1 DataTypesAndVariables/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

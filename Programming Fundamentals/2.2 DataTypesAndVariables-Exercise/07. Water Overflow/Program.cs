09f7699404252d2649b69303a445a19b0dcc1f15
Ali Musa <alimusa_@abv.bg>
2022-07-25 23:39:49 +0300

    Added new exercises to folder 2.2 Data Types And Variables - Exercise

---
+++ Programming Fundamentals/2.2 DataTypesAndVariables-Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CAPACITY = 255;
+            int nLines = int.Parse(Console.ReadLine());
+            int totalQuantity = CAPACITY;
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int currQuantity = int.Parse(Console.ReadLine());
+                if (totalQuantity - currQuantity >= 0)
+                {
+                    totalQuantity -= currQuantity;
+                }
+                else
+                {
+                    Console.WriteLine($"Insufficient capacity!");
+                }
+            }
+
+            int totalFieldQuantity = CAPACITY - totalQuantity;
+
+            Console.WriteLine(totalFieldQuantity);
+        }
+    }
+}

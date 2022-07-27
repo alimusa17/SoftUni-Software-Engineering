95af1c9326b546ca078d91c538a04fc0f3dff163
Ali Musa <alimusa_@abv.bg>
2022-07-27 22:40:54 +0300

    Added new exercises to folder 3.1 Arrays

---
+++ Programming Fundamentals/3.1 Arrays/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenSum += number;
+                }
+                else
+                {
+                    oddSum += number;
+                }
+            }
+
+            Console.WriteLine(evenSum - oddSum);
+        }
+    }
+}

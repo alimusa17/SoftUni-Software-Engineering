bad5c0859409fe023af920e979ab94067b0e5df6
Ali Musa <alimusa_@abv.bg>
2022-07-31 23:28:33 +0300

    Added new exercises to folder 4.1 Methods

---
+++ Programming Fundamentals/4.1 Methods/05. Orders V2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Orders_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPrice(Console.ReadLine(), int.Parse(Console.ReadLine()));
+        }
+
+        static void PrintPrice(string product, int quantity)
+        {
+            switch (product)
+            {
+                case "coffee":
+                    Console.WriteLine($"{quantity * 1.5:f2}");
+                    break;
+                case "water":
+                    Console.WriteLine($"{quantity:f2}");
+                    break;
+                case "coke":
+                    Console.WriteLine($"{quantity * 1.4:f2}");
+                    break;
+                case "snacks":
+                    Console.WriteLine($"{quantity * 2:f2}");
+                    break;
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/4.1 Methods/07. Repeat String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int times = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(input, times));
+        }
+
+        private static string RepeatString(string str, int count)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                //result.Append(str);
+            }
+
+            return result.ToString();
+
+        }
+    }
+}

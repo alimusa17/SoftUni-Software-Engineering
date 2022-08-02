6e9888ca7c5820c39b70779f846407a9cb72fdc2
Ali Musa <alimusa_@abv.bg>
2022-08-02 21:56:21 +0300

    Added new exercises to folder 4.1 Methods

---
+++ Programming Fundamentals/4.1 Methods/11. Math Operations V2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11._Math_Operations_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate(int.Parse(Console.ReadLine()), char.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+        }
+
+        private static double Calculate(int a, char @operator, int b)
+        {
+            return @operator switch
+            {
+                '/' => 1.0 * a / b,
+                '*' => a * b,
+                '+' => a + b,
+                '-' => a - b
+            };
+        }
+    }
+}

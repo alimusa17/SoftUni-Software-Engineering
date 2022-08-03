bad749102991c88f291cc4d6da7cd63d4df3be1c
Ali Musa <alimusa_@abv.bg>
2022-08-03 22:13:58 +0300

    Created new folder 4.2 Methods-Exercise

---
+++ Programming Fundamentals/4.2 Methods-Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine().ToLower();
+
+            SearchForVoweles(inputText);
+        }
+
+        private static void SearchForVoweles(string text)
+        {
+            int count = text.Count(volwe => "aouei".Contains(volwe));
+
+            Console.WriteLine(count);
+        }
+    }
+}

ce67bf7dd9e9a2ea2979e8ca2dc61ddb084f6864
Ali Musa <alimusa_@abv.bg>
2022-08-04 22:55:50 +0300

    Added new exercises to folder 1.2 Intro And Basic Syntax - Exercises

---
+++ Programming Fundamentals/1.2 IntroAndBasicSyntax-Exercise/10. Rage Expenses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keybordPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int trashedHeadset = lostGamesCount / 2;
+            int trashedMouse = lostGamesCount / 3;
+            int trashedKeyboard = trashedMouse / 2;
+            int trashedDisplay = trashedKeyboard / 2;
+
+            double expenses = headsetPrice * trashedHeadset + mousePrice * trashedMouse + keybordPrice * trashedKeyboard + displayPrice * trashedDisplay;
+
+            Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
+        }
+    }
+}
---
+++ Programming Fundamentals/1.2 IntroAndBasicSyntax-Exercise/11. Orders/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counts = int.Parse(Console.ReadLine());
+
+            double totalPrice = 0;
+
+            for (int i = 1; i <= counts; i++)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double price = (days * capsulesCount) * capsulePrice;
+
+                totalPrice += price;
+
+                Console.WriteLine($"The price for the coffee is: ${price:F2}");
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}

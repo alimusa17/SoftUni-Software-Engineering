1bb790b5e18e16e6b6f235cfa0a6935b5fe7aa0e
Ali Musa <alimusa_@abv.bg>
2022-08-05 23:00:59 +0300

    Created new folder 5.1 Lists

---
+++ Programming Fundamentals/5.1 Lists/02. Gauss' Trick V2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Gauss__Trick_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int originalLength = numbers.Count;
+
+            for (int i = 0; i < originalLength / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

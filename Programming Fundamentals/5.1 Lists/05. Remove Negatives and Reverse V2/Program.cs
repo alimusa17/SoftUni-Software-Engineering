496ffd83bc65eb1f82124a31031de7bbc615e9dc
Ali Musa <alimusa_@abv.bg>
2022-08-06 22:48:31 +0300

    Added new exercises to folder 5.1 Lists

---
+++ Programming Fundamentals/5.1 Lists/05. Remove Negatives and Reverse V2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Remove_Negatives_and_Reverse_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            
+            numbers.RemoveAll(number => number < 0);
+            numbers.RemoveAll(number => number >= 0);
+            numbers.RemoveAll(number => number % 2 == 0);
+            
+            numbers.Reverse();
+            
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            
+                return;
+            }
+            
+            Console.WriteLine("empty");
+        }
+    }
+}

db5b056d499ffffd4877de179d21ca0feff2bd51
Ali Musa <alimusa_@abv.bg>
2022-08-08 22:01:49 +0300

    Added new exercises to folder 5.2 Lists - Exercise

---
+++ Programming Fundamentals/5.2 Lists - Exercise/07. Append Arrays V2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> result = new List<string>();
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                var currList = list[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in currList)
+                {
+                    result.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ Programming Fundamentals/5.2 Lists - Exercise/07. Append Arrays V3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays_V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allArrays = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+            List<int> finalList = new List<int>();
+
+            for (int i = allArrays.Length - 1; i >= 0; i--)
+            {
+                int[] tempArray = allArrays[i]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < tempArray.Length; j++)
+                {
+                    finalList.Add(tempArray[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", finalList));
+        }
+    }
+}

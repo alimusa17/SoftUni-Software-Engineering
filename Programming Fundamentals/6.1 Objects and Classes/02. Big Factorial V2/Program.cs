cbbbc396b8d4df5a1235c4590fa5e4590020ef1e
Ali Musa <alimusa_@abv.bg>
2022-08-12 22:52:40 +0300

    Created new folder 6.1 Objects and Classes

---
+++ Programming Fundamentals/6.1 Objects and Classes/02. Big Factorial V2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger f = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                f *= n;
+            }
+
+            Console.WriteLine(f);
+        }
+    }
+}
---
+++ Programming Fundamentals/6.1 Objects and Classes/03. Songs/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> songs = new List<Song>();
+
+            int songCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < songCount; i++)
+            {
+                string[] songProperties = Console.ReadLine().Split('_');
+
+                Song song = new Song(songProperties[0], songProperties[1], songProperties[2]);
+
+                songs.Add(song);
+            }
+
+            string typelist = Console.ReadLine();
+
+            if (typelist == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == typelist)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+
+    class Song
+    {
+        public Song(string typelist, string name, string time)
+        {
+            this.TypeList = typelist;
+            this.Name = name;
+            this.Time = Time;
+        }
+
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+
+
+    }
+}

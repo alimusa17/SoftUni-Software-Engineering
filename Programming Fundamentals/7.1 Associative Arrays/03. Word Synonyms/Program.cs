c0e7e505a78c6be36e8c239b111d3f83331874e1
Ali Musa <alimusa_@abv.bg>
2022-08-16 22:53:58 +0300

    Created new folder 7.1 Associative Arrays

---
+++ Programming Fundamentals/7.1 Associative Arrays/03. Word Synonyms/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonyms.ContainsKey(word))
+                {
+                    synonyms.Add(word, new List<string>());
+                }
+
+                synonyms[word].Add(synonym);
+
+                //List<string> currentSynonyms = synonyms[word];
+                //
+                //currentSynonyms.Add(synonym);
+                //
+                //synonyms[word] = currentSynonyms;
+            }
+
+            foreach (var item in synonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}

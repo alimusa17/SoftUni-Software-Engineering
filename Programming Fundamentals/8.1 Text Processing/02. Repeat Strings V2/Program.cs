2599502f43f2a0272423c350f575563b71357e0d
Ali Musa <alimusa_@abv.bg>
2022-08-19 23:36:37 +0300

    Created new folder 8.1 Text Processing

---
+++ Programming Fundamentals/8.1 Text Processing/02. Repeat Strings V2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            string result = "";
+
+            foreach (string word in words)
+            {
+                int repeatTimes = word.Length;
+
+                for (int i = 0; i < repeatTimes; i++)
+
+                    result += word;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Programming Fundamentals/8.1 Text Processing/03. Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string substringToRemove = Console.ReadLine();
+            string fullString = Console.ReadLine();
+
+            while (fullString.Contains(substringToRemove))
+            {
+                int startIndex = fullString.IndexOf(substringToRemove);
+
+                fullString = fullString.Remove(startIndex, substringToRemove.Length);
+            }
+
+            Console.WriteLine(fullString);
+        }
+    }
+}
+

11315d16c1a23cca7b7d871065cab39074843dba
Ali Musa <alimusa_@abv.bg>
2022-08-20 15:19:57 +0300

    Added new exercises to folder 8.1 Text Processing

---
+++ Programming Fundamentals/8.1 Text Processing/05. Digits, Letters and Other V2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Digits__Letters_and_Other_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] digits = input
+                .Where(ch => char.IsDigit(ch))
+                .ToArray();
+
+            char[] letters = input
+                .Where(ch => char.IsLetter(ch))
+                .ToArray();
+
+            char[] others = input
+                .Where(ch => !char.IsLetterOrDigit(ch))
+                .ToArray();
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
---
+++ Programming Fundamentals/8.1 Text Processing/06. Imitation Game/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _06._Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Decode")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split('|');
+                string action = tokens[0];
+
+                switch (action)
+                {
+                    case "Move":
+                        int length = int.Parse(tokens[1]);
+                        string stringToMove = message.Substring(0, length);
+
+                        message = message.Remove(0, length);
+                        break;
+
+                    case "Insert":
+                        int index = int.Parse(tokens[1]);
+                        string value = tokens[2];
+
+                        message = message.Insert(index, value);
+                        break;
+
+                    case "ChangeAll":
+                        string substring = tokens[1];
+                        string replacement = tokens[2];
+
+                        message = message.Replace(substring, replacement);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"The decrypted message is: {message}"); //TODO SS 6876!!! X 10 && video 2.30.00
+        }
+    }
+}

b7a76a61b2fb69bd1f5b181752ee79caa23bac44
Ali Musa <alimusa_@abv.bg>
2022-09-05 22:52:48 +0300

    Added new exercises to folder  Programming Basics - Exams

---
+++ Programming Basics/Programming Basics - Exams/02. Family Trip/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02._Family_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            double nightPrice = double.Parse(Console.ReadLine());
+            int extraCost = int.Parse(Console.ReadLine());
+
+            if (nights > 7)
+            {
+                nightPrice *= 0.95;
+            }
+
+            double nightsPrice = nights * nightPrice;
+
+            double percentExtraCost = budget * extraCost / 100; 
+
+            double totalPrice = nightsPrice + percentExtraCost;
+
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"Ivanovi will be left with {budget - totalPrice:f2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{(totalPrice - budget):f2} leva needed.");
+            }
+
+        }
+    }
+}

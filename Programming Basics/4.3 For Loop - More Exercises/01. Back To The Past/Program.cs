b9955b9c405ed7e7f6ca1baaabc4208a57d0c0bd
Ali Musa <alimusa_@abv.bg>
2022-09-12 23:40:44 +0300

    Added new exercises to folder 4.3 For Loop - More Exercise

---
+++ Programming Basics/4.3 For Loop - More Exercises/01. Back To The Past/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Back_To_The_Past
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int yearsLiveUpTo = int.Parse(Console.ReadLine());
+
+            double totalMoney = 0;
+            int ages = 18;
+
+            for (int year = 1800; year <= yearsLiveUpTo; year++)
+            {
+                if (year % 2 == 0)
+                {
+                    totalMoney += 12000;
+                }
+                else
+                {
+                    totalMoney += 12000 + 50 * ages;
+                }
+                ages++;
+            }
+
+            if (money >= totalMoney)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {(money - totalMoney):F2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {(totalMoney - money):F2} dollars to survive.");
+            }
+        }
+    }
+}
---
+++ Programming Basics/4.3 For Loop - More Exercises/02. Hospital/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02._Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+
+            int doctors = 7;
+            int treatedPatients = 0;
+            int unTreatedPatients = 0;
+
+            for (int days = 1; days <= period; days++)
+            {
+                int patients = int.Parse(Console.ReadLine());
+
+                if (days % 3 == 0 && unTreatedPatients > treatedPatients)
+                {
+                    doctors++;
+                }
+
+                if (patients >= doctors)
+                {
+                    treatedPatients += doctors;
+                    unTreatedPatients += patients - doctors;
+                }
+                else
+                {
+                    treatedPatients += patients;
+                }
+            }
+
+            Console.WriteLine($"Treated patients: {treatedPatients}.");
+            Console.WriteLine($"Untreated patients: {unTreatedPatients}.");
+
+        }
+    }
+}

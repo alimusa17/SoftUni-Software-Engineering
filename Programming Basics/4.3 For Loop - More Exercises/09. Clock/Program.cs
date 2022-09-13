ca43a3193b7844339914a6b7f4cec4ce1e9e8a8c
Ali Musa <alimusa_@abv.bg>
2022-09-13 21:24:18 +0300

    Added new exercises to folder 4.3 For Loop - More exercises HAPPY PROGRAMMER`S DAY

---
+++ Programming Basics/4.3 For Loop - More Exercises/09. Clock/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09._Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 24 * 60; i++)
+            {
+                Console.WriteLine($"{i / 60} : {i % 60}");
+            }
+        }
+    }
+}
---
+++ Programming Basics/4.3 For Loop - More Exercises/10. Clock - part 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._Clock___part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hours = 0; hours < 24; hours++)
+            {
+                for (int minutes = 0; minutes < 60; minutes++)
+                {
+                    for (int seconds = 0; seconds < 60; seconds++)
+                    {
+                        Console.WriteLine($"{hours} : {minutes} : {seconds}");
+                    }
+                }
+            }
+        }
+    }
+}

166868b05643f82eaa686ee42c757a66b78c34e8
Ali Musa <alimusa_@abv.bg>
2022-09-15 23:11:54 +0300

    Created new folder 6.3  Nested Loops - More Exercise

---
+++ Programming Basics/6.3 Nested Loops - More Exercises/01. Unique PIN Codes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _01._Unique_PIN_Codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            for (int firstOne = 1; firstOne <= first; firstOne++)
+            {
+                for (int secondOne = 1; secondOne <= second; secondOne++)
+                {
+                    for (int thirdOne = 1; thirdOne <= third; thirdOne++)
+                    {
+                        int firstNum = 0, secondNum = 0, thirdNum = 0;
+
+                        if (firstOne % 2 == 0)
+                        {
+                            firstNum = firstOne;
+                        }
+                        if (secondOne == 2 || secondOne == 3 || secondOne == 5 || secondOne == 7)
+                        {
+                            secondNum = secondOne;
+                        }
+                        if (thirdOne % 2 == 0)
+                        {
+                            thirdNum = thirdOne;
+                        }
+
+                        if (firstNum != 0 && secondNum != 0 && thirdNum != 0)
+                        {
+                            Console.WriteLine($"{firstNum} {secondNum} {thirdNum}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming Basics/6.3 Nested Loops - More Exercises/05. Challenge the/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Challenge_the_Wedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gentleman = int.Parse(Console.ReadLine());
+            int ladies = int.Parse(Console.ReadLine());
+            int tables = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int i = 1; i <= gentleman; i++)
+            {
+                for (int l = 1; l <= ladies; l++)
+                {
+                    counter++;
+
+                    if (counter <= tables)
+                    {
+                        Console.Write($"({i} <-> {l}) ");
+                    }
+                }
+            }
+        }
+    }
+}

2df76ca5c24d95a9133a1e93ad38f45a84a3393f
Ali Musa <alimusa_@abv.bg>
2022-09-16 22:11:13 +0300

    Added new exercises to folder 6.3 Nested Loops - More exercises

---
+++ Programming Basics/6.3 Nested Loops - More Exercises/08. Secret Door's/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08._Secret_Door_s_Lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num1; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    continue;
+                }
+                for (int k = 1; k <= num2; k++)
+                {
+                    if (k == 1 || k == 4 || k == 6 || k == 8 || k == 9)
+                    {
+                        continue;
+                    }
+                    for (int j = 1; j <= num3; j++)
+                    {
+
+                        if (j % 2 != 0)
+                        {
+                            continue;
+                        }
+                        Console.WriteLine($"{i} {k} {j}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming Basics/6.3 Nested Loops - More Exercises/10. Profit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10._Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coins1 = int.Parse(Console.ReadLine());
+            int coins2 = int.Parse(Console.ReadLine());
+            int coins5 = int.Parse(Console.ReadLine());
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= coins1; i++)
+            {
+                for (int k = 0; k <= coins2; k++)
+                {
+                    for (int l = 0; l <= coins5; l++)
+                    {
+                        if ((i * 1) + (k * 2) + (l * 5) == sum)
+                        {
+                            Console.WriteLine($"{i} * 1 lv. + {k} * 2 lv. + {l} * 5 lv. = {sum} lv.");
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}

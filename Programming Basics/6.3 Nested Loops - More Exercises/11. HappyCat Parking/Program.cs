2c831567f3f3b378e6bbefb0cfdb698d2262e330
Ali Musa <alimusa_@abv.bg>
2022-09-17 22:30:38 +0300

    Added new exercises to folder 6.3 Nested Loops - More Exercises

---
+++ Programming Basics/6.3 Nested Loops - More Exercises/11. HappyCat Parking/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _11._HappyCat_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double days = double.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+
+            double sumForDay = 0;
+            double totalSum = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                sumForDay = 0;
+
+                for (int k = 1; k <= hours; k++)
+                {
+                    if (i % 2 == 0 && k % 2 != 0)
+                    {
+                        sumForDay += 2.50;
+                    }
+                    else if (i % 2 != 0 && k % 2 == 0)
+                    {
+                        sumForDay += 1.25;
+                    }
+                    else
+                    {
+                        sumForDay += 1;
+                    }
+                }
+
+                totalSum += sumForDay;
+                Console.WriteLine($"Day: {i} - {sumForDay:F2} leva");
+            }
+            Console.WriteLine($"Total: {totalSum:F2} leva");
+        }
+    }
+}
---
+++ Programming Basics/6.3 Nested Loops - More Exercises/13. Prime Pairs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _13._Prime_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum1 = int.Parse(Console.ReadLine());
+            int startNum2 = int.Parse(Console.ReadLine());
+            int endNum1 = int.Parse(Console.ReadLine());
+            int endNum2 = int.Parse(Console.ReadLine());
+
+            for (int a = startNum1; a <= startNum1 + endNum1; a++)
+            {
+                for (int k = startNum2; k <= startNum2 + endNum2; k++)
+                {
+                    bool isPrime1 = true;
+                    for (int i = 2; i <= Math.Sqrt(a); i++)
+                    {
+                        if (a % i == 0)
+                        {
+                            isPrime1 = false;
+                        }
+                    }
+                    bool isPrime2 = true;
+                    for (int i = 2; i <= Math.Sqrt(k); i++)
+                    {
+                        if (k % i == 0)
+                        {
+                            isPrime2 = false;
+                        }
+                    }
+                    if (isPrime1 && isPrime2)
+                    {
+                        Console.WriteLine($"{a}{k}");
+                    }
+                }
+            }
+
+        }
+    }
+}
